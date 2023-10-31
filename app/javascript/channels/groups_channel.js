ebc3fb73e084a2ad6fbdad0b5a3c35e4242a2b84
Armin Kirchner <post.armin@gmail.com>
2023-10-31 10:33:59 +0900

    JavaScript for group page

--- app/javascript/channels/groups_channel.js
+++ app/javascript/channels/groups_channel.js
@@ -1,16 +1,24 @@
 import consumer from "channels/consumer"
 
-consumer.subscriptions.create("GroupsChannel", {
-  connected() {
-    console.log("Connected to the groups channel!");
-  },
-  disconnected() {
-    console.log("Disconnected from the groups channel!");
-  },
-  rejected() {
-    console.log("Rejected from the groups channel!");
-  },
-  received(data) {
-    console.log("Groups channel received data", data);
+addEventListener("turbo:load", () => {
+  const element = document.getElementById("groups-channel")
+
+  if (element !== null) {
+    consumer.subscriptions.create("GroupsChannel", {
+      connected() {
+        console.log("Connected to the groups channel!");
+      },
+      disconnected() {
+        console.log("Disconnected from the groups channel!");
+      },
+      rejected() {
+        console.log("Rejected from the groups channel!");
+      },
+      received(data) {
+        const li = document.createElement("li")
+        li.innerHTML = data
+        element.append(li);
+      }
+    });
   }
 });
